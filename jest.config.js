c8a94c20d12e679fc1f2e493bd704626e04e33c7
Tweek <vallkhale@gmail.com>
2020-05-15 08:25:15 +0300

    jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    clearMocks: true,
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.ts"],
+    coverageReporters: ["lcov", "html", "text"],
+    coverageDirectory: "coverage",
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+    ],
+    moduleDirectories: [
+        "node_modules"
+    ],
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(js?)$",
+    moduleFileExtensions: ["js"],
+    testEnvironment: "node",
+    transformIgnorePatterns: [
+        "/node_modules/"
+    ],
+};
