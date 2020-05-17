f7e2cdc75018e5f4a6107837da63d563a1ae6912
Tweek <vallkhale@gmail.com>
2020-05-17 11:17:23 +0300

    clean

--- pick-peaks/index.spec.js
+++ pick-peaks/index.spec.js
@@ -16,10 +16,3 @@ describe("Pick Peaks", function() {
         expect(pickPeaks([0, 1, 2])).toEqual({pos:[], peaks:[]})
     });
 });
-
-// function pickPeaks(arr){
-//   var pos = arr.map((x,i)=>i > 0 ? Math.sign(x - arr[i-1]) * i : 0)
-//     .filter(i => i != 0)
-//     .filter((x,i,a) => i < a.length-1 && (a[i+1] < 0 && x > 0));
-//   return {pos:pos, peaks:pos.map(i=>arr[i])}
-// }
