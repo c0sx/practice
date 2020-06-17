1921b1a59ab95a05a07e2ad99cac72113e6a59c2
Tweek <vallkhale@gmail.com>
2020-06-17 19:22:32 +0300

    +

---
+++ - josephus-permutation/index.js
@@ -0,0 +1,24 @@
+function josephus(items, k) {
+    if (items.length === 0) {
+        return [];
+    }
+
+    const results = [];
+    let step = 1;
+    while (items.length > 0) {
+        for (let i = 0; i < items.length; ++i) {
+            if (step % k === 0) {
+                results.push(items[i])
+                items[i] = undefined;
+            }
+
+            step++;
+        }
+
+        items = items.filter(Boolean)
+    }
+
+    return results;
+}
+
+module.exports = josephus;
---
+++ - josephus-permutation/index.spec.js
@@ -0,0 +1,11 @@
+const josephus = require("./")
+
+describe("Josephus Permutation", () => {
+    it("tests", () => {
+        expect(josephus([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        expect(josephus([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)).toEqual([2, 4, 6, 8, 10, 3, 7, 1, 9, 5])
+        expect(josephus(["C", "o", "d", "e", "W", "a", "r", "s"], 4)).toEqual(["e", "s", "W", "o", "C", "d", "r", "a"])
+        expect(josephus([1, 2, 3, 4, 5, 6, 7], 3)).toEqual([3, 6, 2, 7, 5, 1, 4])
+        expect(josephus([], 3)).toEqual([])
+    });
+})
