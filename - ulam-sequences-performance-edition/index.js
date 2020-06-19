fba78b911d32deecbba430ff42e492fc65d4cb4c
Tweek <vallkhale@gmail.com>
2020-06-19 16:51:13 +0300

    +

--- - ulam-sequences-performance-edition/index.js
+++ - ulam-sequences-performance-edition/index.js
@@ -1,95 +1,90 @@
 function ulamSequence(u0, u1, n) {
-    const sequence = [u0, u1];
+    const used = new Set()
+    let left = [u0, u1];
+    let right = [u0 + u1];
+    let remains = [];
 
     for (let i = 0; i < n - 2; ++i) {
-        const next = findSum2(sequence);
-        sequence.push(next);
+        [left, remains, right] = getSums(left, remains, right, used);
+        const kek = 5;
     }
 
-    return sequence
+    return left;
 }
 
-function findSum2(sequence) {
-    let rightIterator = sequence.length - 1;
-    let leftIterator = 0;
-    let i = 0;
+const getSums = (left, remains, right, used) => {
+    const sums = [];
 
-    const map = new Map();
-    while (leftIterator < rightIterator) {
-        iter(map, sequence, i)
-
-        i++;
-        leftIterator = i + 1;
-        rightIterator = sequence.length - (i + 2)
-    }
-
-    const values = [...map.entries()]
-        .filter(([key, value]) => value === 1)
-        .map(([key]) => key);
-
-    return Math.min(...values);
-}
-
-function iter(map, sequence, i) {
-    let state = 0;
-    const start = i;
-    const end = sequence.length - (i + 1);
-
-    while (state < 4) {
-        const [leftIndex, rightIndex] = getNextIterators(start, end, state)
-        if (leftIndex >= rightIndex) {
-            return;
+    // нужно не складывать уже складываемые рание слагаемые
+    for (let i = 0; i < right.length; ++i) {
+        const rightTerm = right[i];
+        if (rightTerm === undefined) {
+            continue;
         }
 
-        const left = sequence[leftIndex];
-        const right = sequence[rightIndex];
-        const sum = left + right;
-        if (!sequence.includes(sum)) {
-            const counter = map.get(sum) || 0;
-            map.set(sum, counter + 1);
-        }
+        left.forEach(leftTerm => {
+            const sum = leftTerm + rightTerm;
+            if (used.has(sum)) {
+                const sumsIndex = sums.indexOf(sum);
+                if (sumsIndex !== -1) {
+                    sums.splice(sumsIndex, 1)
+                }
 
-        state++;
-    }
-}
+                const rightIndex = right.indexOf(sum);
+                if (rightIndex !== -1) {
+                    right[rightIndex] = undefined
+                    const kek = 10;
+                }
 
-function getNextIterators(left, right, state) {
-    switch (state) {
-        case 0: return [left, right];
-        case 1: return [left + 1, right];
-        case 2: return [left, right - 1];
-        case 3: return [left + 1, right - 1]
-    }
-}
-
-function findMinSumInSequence(sequence) {
-    const sums = sequence.reduce((map, current, index, sequence) => {
-
-        for (let i = index + 1; i < sequence.length; ++i) {
-            const one = sequence[i]
-            const sum = current + one;
-            if (sequence.includes(sum)) {
-                continue;
+                return;
             }
 
-            const counter = map.get(sum) || 0;
-            map.set(sum, counter + 1)
-        }
-
-        return map;
-    }, new Map());
-
-    const values = [...sums.entries()]
-        .reduce((acc, [key, value]) => {
-            if (value === 1) {
-                acc.push(key)
+            // если сумма уже существует в правом срезе, удалить это значение из среза
+            const rightIndex = right.indexOf(sum);
+            if (rightIndex !== -1) {
+                right[rightIndex] = undefined;
+                const kek = rightIndex;
+                used.add(sum)
             }
 
+            // если сумма уже существует в собранных суммах, то удалить ее оттуда
+            // (а удалять вроде бы нельзя потому что в каком-то кейсе она повторно появится)
+            const sumsIndex = sums.indexOf(sum);
+            if (sumsIndex === -1 && rightIndex === -1) {
+                sums.push(sum)
+            }
+            else if (sumsIndex !== -1) {
+                sums.splice(sumsIndex, 1)
+                used.add(sum)
+            }
 
-            return acc;
-        }, []);
+            // удалить так же из остатков
+            const remainsIndex = remains.indexOf(sum);
+            if (remainsIndex !== -1) {
+                remains.splice(remainsIndex, 1)
+                used.add(sum)
 
-    return Math.min(...values);
+                const index = sums.indexOf(sum);
+                sums.splice(index, 1);
+            }
+        });
+    }
+
+    const total = [...remains, ...right, ...sums].sort((a, b) => a - b);
+    const first = total.shift();
+    left.push(first);
+
+    const remainsIndex = remains.indexOf(first);
+    if (remainsIndex !== -1) {
+        remains.splice(remainsIndex, 1);
+    }
+
+    const rightIndex = right.indexOf(first);
+    if (rightIndex !== -1) {
+        right.splice(rightIndex, 1);
+    }
+
+    return [left, right.filter(one => one !== undefined), sums]
 }
 
 module.exports = ulamSequence
