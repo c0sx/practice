64b09000368e5803093a2047847437f2e816505d
c0sx <vallkhale@gmail.com>
2020-06-22 00:44:54 +0300

    +

--- - simple-fun-338-the-sum-of-prime-distance/index.js
+++ - simple-fun-338-the-sum-of-prime-distance/index.js
@@ -1,10 +1,66 @@
 function sumOfPrimeDistance(arr) {
-    const pairs = getPairs(arr);
+    const pairs = getPairs2(arr);
+    const primed = calculatePrimes(pairs);
 
-    return pairs.reduce((acc, [left, right]) => {
+    const length = arr.length - 1;
+    const total = length * length / 2
+
+    let step = 1;
+    while (step < total) {
+        for (let i = 0; i < primed.length - 1; ++i) {
+            const left = primed[i];
+            const right = primed[i + step];
+
+            const merged = merge(left, right);
+            primed.push(merged)
+        }
+
+        step++;
+    }
+
+    return sum;
+    //
+    // for (let i = 0; i < arr.length - 1; i++) {
+    //     acc.push([current, original[step]])
+    // }
+
+
+    // return primed;
+
+    //     const length = arr.length - 1;
+    //     const total = length * length / 2
+
+    // const pairs = getPairs(arr);
+    // return pairs.reduce((acc, [left, right]) => {
+    //     const primes = getPrimesInInterval(left, right);
+    //     return acc + primes.length;
+    // }, 0)
+}
+
+const merge = (left, right) => {
+    const [ll, lr, lp] = left;
+    const [rl, rr, rp] = right;
+
+    return [ll, rr, lp + rp];
+}
+
+const getPairs2 = arr => {
+    const pairs = [];
+    for (let i = 0; i < arr.length - 1; ++i) {
+        const current = arr[i];
+        const next = arr[i + 1];
+
+        pairs.push([current, next])
+    }
+
+    return pairs;
+}
+
+const calculatePrimes = pairs => {
+    return pairs.map(([left, right]) => {
         const primes = getPrimesInInterval(left, right);
-        return acc + primes.length;
-    }, 0)
+        return [left, right, primes.length]
+    });
 }
 
 const getPairs = arr => {
