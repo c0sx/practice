05a4b978627fef3dd4e55204c74b2554ee805d33
c0sx <vallkhale@gmail.com>
2020-06-23 00:57:00 +0300

    +

--- - simple-fun-338-the-sum-of-prime-distance/index.js
+++ - simple-fun-338-the-sum-of-prime-distance/index.js
@@ -1,41 +1,58 @@
 function sumOfPrimeDistance(arr) {
-    const pairs = getPairs2(arr);
+    console.time("pairs")
+    const pairs = getPairs(arr);
+    console.timeEnd("pairs");
+
+    console.time("primed");
     let primed = calculatePrimes(pairs);
+    console.timeEnd("primed")
 
-    let total = sumRow(primed);
-    let step = 1;
+    console.time("counter")
+    let step = 0;
+    const row = [];
 
-    while (step < primed.length) {
-        for (let i = 0; i < primed.length - step; ++i) {
-            const [left] = primed[i];
-            const [_, right] = primed[i + step];
-
-            const slice = merge2(primed, left, right);
-            total += sumRow(slice)
+    // if arr.length % 2 === 0 -- middle === primed.length / 2
+    // if arr.length % 2 === 1 -- middle === primed.length / 2, но middle 2 значения
+    while (step < primed.length / 2) { // каждый ряд прямоугольника
+        let rowSum = 0;
+        for (let i = 0; i < primed.length - step; ++i) { // скашиваем прямоугольник до треугольника
+            for (let k = i; k < i + step + 1; ++k) { // считаем каждый отрезок в строке треугольника
+                rowSum += primed[k]
+            }
         }
 
+        row.push(rowSum)
         step++;
     }
 
+    console.timeEnd("counter")
+
+    console.time("result")
+    if (arr.length % 2 === 0) {
+        const last = row.pop();
+        const total = last + row.reduce((acc, curr) => acc + curr, 0) * 2
+        console.timeEnd("result");
+        return total;
+    }
+
+    const last = row.pop();
+    row.pop();
+
+    const total = last * 2 + row.reduce((acc, curr) => acc + curr, 0) * 2
+    console.timeEnd("result");
     return total;
 }
 
-const sumRow = row => row.reduce((acc, [l, r, p]) => acc + p, 0)
+const sumRow = row => {
+    let sum = 0;
+    for (let i = 0; i < row.length; ++i) {
+        sum += row[i]
+    }
 
-const merge2 = (row, left, right) => {
-    const start = row.findIndex(([a]) => a === left);
-    const end = row.findIndex(([a, b]) => b === right);
-    return row.slice(start, end + 1);
+    return sum;
 }
 
-const merge = (left, right) => {
-    const [ll, lr, lp] = left;
-    const [rl, rr, rp] = right;
-
-    return [ll, rr, lp + rp];
-}
-
-const getPairs2 = arr => {
+const getPairs = arr => {
     const pairs = [];
     for (let i = 0; i < arr.length - 1; ++i) {
         const current = arr[i];
@@ -50,7 +67,7 @@ const getPairs2 = arr => {
 const calculatePrimes = pairs => {
     return pairs.map(([left, right]) => {
         const primes = getPrimesInInterval(left, right);
-        return [left, right, primes.length]
+        return primes.length;
     });
 }
 
