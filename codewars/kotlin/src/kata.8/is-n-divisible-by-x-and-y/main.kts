e2cd073f2835d9a1c2a48965e5e2c854fcb2b6a5
tweek <vallkhale@gmail.com>
2021-01-22 21:52:27 +0300

    refactor

--- codewars/kotlin/src/kata.8/is-n-divisible-by-x-and-y/main.kts
+++
@@ -1,8 +0,0 @@
-fun isDivisible(n: Int, x: Int, y: Int): Boolean = n % x == 0 && n % y == 0
-
-println(isDivisible(3, 3, 4)) // false
-println(isDivisible(12, 3, 4)) // true
-println(isDivisible(8, 3, 4)) // false
-println(isDivisible(48, 3, 4)) // true
-println(isDivisible(100, 5, 10)) // true
-println(isDivisible(100, 5, 3)) // false
