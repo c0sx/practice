3e9df95887404fa55f85144a3fd24eeff30893dc
c0sx <c0sx@users.noreply.github.com>
2023-11-10 22:46:34 +0400

    feat(leetcode): restore array

--- leetcode/restore-the-array-from-adjacent-pairs/js/index.js
+++ leetcode/restore-the-array-from-adjacent-pairs/js/index.js
@@ -1,26 +1,23 @@
 class Graph {
   constructor() {
     this.nodes = new Map();
-    this.edges = new Set();
   }
 
   link(from, to) {
-    const nodes = this.nodes.get(from) ?? new Set();
-    nodes.add(to);
-
-    if (nodes.size < 2) {
-      this.edges.add(from);
-    } else {
-      this.edges.delete(from);
-    }
+    const nodes = this.nodes.get(from) ?? [];
+    nodes.push(to);
 
     this.nodes.set(from, nodes);
   }
 
   traverse() {
+    const root = [...this.nodes.keys()].find(
+      (key) => this.nodes.get(key).length === 1
+    );
+
     const list = new Set();
 
-    this.edges.forEach((key) => this._traverse(key, list));
+    this._traverse(root, list);
 
     return list;
   }
