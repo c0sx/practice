6d83790ad21556c4b035102a21e75388ff6b9d68
c0sx <c0sx@users.noreply.github.com>
2025-02-11 22:28:30 +0400

    feat(leetcode): remove-all-occurrences-of-a-string

---
+++ leetcode/remove-all-occurrences-of-a-string/rust/src/lib.rs
@@ -0,0 +1,44 @@
+pub fn remove_occurrences(s: String, part: String) -> String {
+    let mut chars: Vec<char> = s.chars().collect();
+    let needle: Vec<char> = part.chars().collect();
+
+    let mut i: usize = 0;
+    let mut j: usize = 0;
+
+    while i < chars.len() {
+        while i + j < chars.len() && j < needle.len() && chars[i + j] == needle[j] {
+            j += 1;
+        }
+
+        if j == needle.len() {
+            chars.drain(i..i + j);
+
+            i = if j > i { 0 } else { i - j }
+        } else {
+            i += 1;
+        }
+
+        j = 0;
+    }
+
+    chars.iter().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works1() {
+        let result = remove_occurrences(String::from("daabcbaabcbc"), String::from("abc"));
+
+        assert_eq!(result, "dab");
+    }
+
+    #[test]
+    fn it_works2() {
+        let result = remove_occurrences(String::from("axxxxyyyyb"), String::from("xy"));
+
+        assert_eq!(result, "ab");
+    }
+}
