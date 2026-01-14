3c7566fc5ce920ee117b028ddc21754319ac2959
Koshkin Nikolai <c0sx@users.noreply.github.com>
2026-01-14 17:50:15 +0400

    feat(leetcode): daily

---
+++ leetcode/3454. separate-squares-ii/go/go.mod
@@ -0,0 +1,3 @@
+module leetcode3454
+
+go 1.25.1
---
+++ leetcode/3454. separate-squares-ii/go/main_test.go
@@ -0,0 +1,25 @@
+package leetcode3454
+
+import "testing"
+
+func TestSeparateSquares(t *testing.T) {
+	tests := []struct {
+		squares [][]int
+		want    float64
+	}{
+		{
+			squares: [][]int{{0, 0, 1}, {2, 2, 1}},
+			want:    1.0,
+		},
+		{
+			squares: [][]int{{0, 0, 2}, {1, 1, 1}},
+			want:    1.0,
+		},
+	}
+
+	for _, tt := range tests {
+		if got := separateSquares(tt.squares); got != tt.want {
+			t.Errorf("separateSquares(%v) = %v, want %v", tt.squares, got, tt.want)
+		}
+	}
+}
